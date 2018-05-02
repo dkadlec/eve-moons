0e30fbb4421781eef4963cf7ec2969a44fb1fbd2
David Kadlec <davidkadlec93@gmail.com>
2018-05-02 23:46:04 +0200

    Add styles, highlight good ores

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,49 @@
 var express = require('express');
 var router = express.Router();
 
+const precisionRound = (number, precision) => {
+  const factor = Math.pow(10, precision);
+  return Math.round(number * factor) / factor;
+}
+
+const GOOD_ORES = ['Flawless Arkonor', 'Cubic Bistot', 'Pellucid Crokite', 'Dazzling Spodumain', 'Jet Ochre'];
+
 const processData = (string) => {
-  let alois = null;
+  let header = null;
 
-  result = string.split('\n').reduce((result, curr) => {
-    let a = curr.replace('\r', ''); // remove \r from string
-    a = curr.split('\t'); // split by tab chars
+  let result = string.split('\n');
 
-    if (a.length === 1) {
-      alois = a[0];
+  if (result[0].split('\t')[0] === 'Moon' && result[0].split('\t')[1] === 'Moon Product') {
+    result = result.splice(1); // ommit first line with column names
+  }
 
-      result[alois] = [];
+  result = result.reduce((result, curr) => {
+    curr = curr.replace('\r', ''); // remove \r from string
+    curr = curr.split('\t'); // split by tab chars
+
+    if (curr.length === 1) { // is a moon name
+      header = curr[0];
+
+      result[header] = [];
 
       return result;
     } else {
-      a = a.splice(1);
-      const [ product, quantity, oreId, solarSystemId, planetId, moonId] = a;
+      curr = curr.splice(1);
+      const [ oreName, quantity, oreId, solarSystemId, planetId, moonId] = curr;
+
+      const isGood = GOOD_ORES.includes(oreName);
+
       const b = {
-        product,
-        quantity,
+        oreName,
+        quantity: precisionRound(quantity, 2),
         oreId,
         solarSystemId,
         planetId,
         moonId,
-        addedAt: +(new Date())
+        isGood,
       };
 
-      result[alois].push(b);
+      result[header].push(b);
 
       return result;
     }
@@ -35,17 +51,15 @@ const processData = (string) => {
     return result;
   }, {});
 
-  return [JSON.stringify(result), result];
+  return result;
 }
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express', data: 'no data' });
 });
 
 router.post('/add', function(req, res, next) {
-  const table = processData(req.body.moondata);
-  res.render('index', {title: 'express', script: table[0], table: table[1]});
+  res.render('index', {title: 'express', table: processData(req.body.moondata)});
 });
 
 module.exports = router;
